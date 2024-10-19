e66e7592fc597cfa4cb2431ad40627286a788c79
Ana-Maria Radu <ana.radu@uipath.com>
2024-10-19 13:53:15 +0300

    Initial commit

---
+++ OnlineFormProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineFormProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
